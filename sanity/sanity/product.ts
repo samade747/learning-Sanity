57423d5c9d01e4fe4c56958faba62066323b9622
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-12-12 04:41:08 +0500

    sscsa

---
+++ sanity/sanity/product.ts
@@ -0,0 +1,34 @@
+export const product = {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: { 
+                source: 'title',
+                maxLength: 96,
+            },
+        },
+        {   
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        }
+    ],
+}
