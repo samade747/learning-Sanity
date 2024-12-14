336b756e03789832306591678b098ad00082d62a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-12-14 06:48:51 +0500

    push 1113

--- sanity/sanity/lib/client.ts
+++ sanity/sanity/lib/client.ts
@@ -3,8 +3,12 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2024-12-03",
+  token: process.env.SANITY_API_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
+
+
+// await client.fetch(`*[_type == "product"]`);
